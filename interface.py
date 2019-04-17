48b8dd821d012f95bac5b5c5c2641c0c3707de49
Rik <Rik@Heijmann.com>
2019-04-17 13:13:44 +0200

    Nette inlogfoutmelding toegevoegd

--- interface.py
+++ interface.py
@@ -3,6 +3,18 @@ import tabtest
 import dbtest
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QMessageBox
+
+
+def foutmelding(title, text, informativetext):
+    msg = QMessageBox()
+    msg.setIcon(QMessageBox.Warning)
+
+    msg.setText(text)
+    msg.setInformativeText(informativetext)
+    msg.setWindowTitle(title)
+    msg.setStandardButtons(QMessageBox.Ok)
+    msg.exec_()
 
 
 class Loginscherm(login.Ui_MainWindow):
@@ -11,6 +23,8 @@ class Loginscherm(login.Ui_MainWindow):
         self.setupUi(MainWindow)
         self.pushButton.clicked.connect(self.login)
 
+
+
     def login(self):
         loginresult = dbtest.login(self.lineEdit.text(
         ), self.lineEdit_2.text(), self.lineEdit_3.text())
@@ -18,7 +32,7 @@ class Loginscherm(login.Ui_MainWindow):
             mainscreen = tabtest.Ui_MainWindow(loginresult)
             mainscreen.setupUi(MainWindow)
         else:
-            print("WONG")
+            foutmelding("Incorrecte inloggegevens", "Een van de inloggegevens is incorrect", "Controleert u alstublieft of alle ingevoerde gegevens correct zijn. Neem anders contact op met het ICT-servicedesk.")
 
 
 app = QtWidgets.QApplication(sys.argv)
